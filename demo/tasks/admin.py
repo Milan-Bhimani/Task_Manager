237edb14705281839a1402efe13cf2d2f03ff6a6
Milan BHimani <milanhbhimani@gmail.com>
2026-01-06 17:55:29 +0530

    added models to admin panel so i can directly add them properly

--- demo/tasks/admin.py
+++ demo/tasks/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Project, Task
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ("name", "owner", "start_date")
+    search_fields = ("name", "owner__username")
+    list_filter = ("start_date",)
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("title", "project", "priority", "status", "due_date")
+    search_fields = ("title", "project__name")
+    list_filter = ("priority", "status", "due_date")
+
